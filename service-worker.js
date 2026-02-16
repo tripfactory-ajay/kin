9480b1b9b7cb6ab0a4c41200c57b185a839bb5f5
tripfactory-ajay <ajay.kawa@tripfactory.com>
2026-02-16 16:28:53 +0530

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,1 @@
+const CACHE_NAME='kin-v1',urlsToCache=['/kin/','/kin/index.html','/kin/kin.png'];self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(urlsToCache)))});self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))});
